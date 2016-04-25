e83a51ade50ce4ce797f42ef6774736e50942b04
Sunny Gurnani <gurnani.sunny@gmail.com>
2016-04-25 13:20:19 -0700

    Reset and added upload to S3

--- ios/RCTCognito/RCTCognito.m
+++ ios/RCTCognito/RCTCognito.m
@@ -7,13 +7,10 @@
 //
 
 #import "RCTBridge.h"
-#import "RCTConvert.h"
 #import "RCTCognito.h"
 
 #import <AWSCognito/AWSCognito.h>
 #import <AWSCore/AWSCore.h>
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <AWSS3/AWSS3.h>
 
 typedef AWSRegionType (^CaseBlock)();
 
@@ -23,47 +20,40 @@ typedef AWSRegionType (^CaseBlock)();
 RCT_EXPORT_MODULE();
 
 - (AWSRegionType)getRegionFromString:(NSString *)region {
-    NSDictionary *regions = @{
-                              @"eu-west-1" : ^{
-                                  return AWSRegionEUWest1;
-                              },
-                              @"us-east-1" : ^{
-                                  return AWSRegionUSEast1;
-                              },
-                              @"ap-northeast-1" : ^{
-                                  return AWSRegionAPNortheast1;
-                              },
-                              };
-    return ((CaseBlock)regions[region])();
+  NSDictionary *regions = @{
+    @"eu-west-1" : ^{
+      return AWSRegionEUWest1;
+    },
+    @"us-east-1" : ^{
+      return AWSRegionUSEast1;
+    },
+    @"ap-northeast-1" : ^{
+      return AWSRegionAPNortheast1;
+    },
+  };
+  return ((CaseBlock)regions[region])();
 }
 
- NSString *identityPoolId = @"";
- NSString *region = @"";
+RCT_EXPORT_METHOD(initCredentialsProvider: (NSString *)identityPoolId
+                  : (NSString *)token
+                  : (NSString *)region) {
+  AWSCognitoCredentialsProvider *credentialsProvider =
+      [[AWSCognitoCredentialsProvider alloc]
+          initWithRegionType:[self getRegionFromString:region]
+              identityPoolId:identityPoolId];
 
+  credentialsProvider.logins = @{
+    @(AWSCognitoLoginProviderKeyFacebook) : token
+  };
 
-- (AWSCognitoCredentialsProvider *) StartCognito: (NSString *)token {
-    AWSCognitoCredentialsProvider *credentialsProvider =
-    [[AWSCognitoCredentialsProvider alloc]
-     initWithRegionType:[self getRegionFromString:region]
-     identityPoolId:identityPoolId];
+  AWSServiceConfiguration *configuration = [[AWSServiceConfiguration alloc]
+           initWithRegion:[self getRegionFromString:region]
+      credentialsProvider:credentialsProvider];
 
-
-    if([token length] != 0) {
-
-        credentialsProvider.logins = @{
-                                       @"graph.facebook.com" : token
-                                       };
-    }
-
-    AWSServiceConfiguration *configuration = [[AWSServiceConfiguration alloc]
-                                              initWithRegion:[self getRegionFromString:region]
-                                              credentialsProvider:credentialsProvider];
-
-    [AWSServiceManager defaultServiceManager].defaultServiceConfiguration =
-    configuration;
-
-    return credentialsProvider;
+  [AWSServiceManager defaultServiceManager].defaultServiceConfiguration =
+      configuration;
 }
+
 
 RCT_REMAP_METHOD(UploadFileToS3,
                       : (NSString*) fileUrl
@@ -111,136 +101,39 @@ RCT_REMAP_METHOD(UploadFileToS3,
 
 }
 
-
-
-
-RCT_EXPORT_METHOD(initCredentialsProvider: (NSString *)identityPoolIdInput
-                  : (NSString *)tokenInput
-                  : (NSString *)regionInput
-                  ) {
-
-    identityPoolId =identityPoolIdInput;
-    region = regionInput;
-    [self StartCognito: tokenInput];
-
-}
-
-RCT_EXPORT_METHOD(clearCognitoCredentialsProvider)
-{
-    AWSCognitoCredentialsProvider *credentialsProvider = [[[AWSCognito defaultCognito] configuration] credentialsProvider];
-    [credentialsProvider clearCredentials];
-    //[credentialsProvider clearKeychain];
-}
-
-RCT_EXPORT_METHOD(setVariables: (NSString *)identityPoolIdInput
-                  :(NSString *)regionInput
-                  )
-{
-    identityPoolId = identityPoolIdInput;
-    region = regionInput;
-}
-
-
-RCT_REMAP_METHOD(getCognitoCredentials,
-                 resolver:(RCTPromiseResolveBlock)resolve
-                 rejecter:(RCTPromiseRejectBlock)reject)
-{
-    AWSCognitoCredentialsProvider *credentialsProvider = [[[AWSCognito defaultCognito] configuration] credentialsProvider];
-
-    NSString *token = [FBSDKAccessToken currentAccessToken].tokenString;
-    Boolean isAuthenticated = [token length] != 0;
-
-
-    if(credentialsProvider == nil){
-        [self StartCognito:token];
-        credentialsProvider = [[[AWSCognito defaultCognito] configuration] credentialsProvider];
-    }
-    else{
-       // if(isAuthenticated)
-
-            NSMutableDictionary *merge = [NSMutableDictionary dictionaryWithDictionary:credentialsProvider.logins];
-            NSString *currentToken = [merge valueForKey: @"graph.facebook.com"];
-
-
-
-
-        if([currentToken length] != 0 && !isAuthenticated){
-            //If User Logged Out from Facebook and Currently Logged In to Cognito
-            [merge removeObjectForKey:@"graph.facebook.com"];
-            [[AWSCognito defaultCognito] wipe];
-            [credentialsProvider clearKeychain];
-
-            //Re Initialize Credentials Provider so we get UnAuthenticated User Credentials
-            [self StartCognito:nil];
-            credentialsProvider = [[[AWSCognito defaultCognito] configuration] credentialsProvider];
-
-
-        }
-
-
-        else if( isAuthenticated && ([currentToken length] == 0 || ![currentToken isEqualToString: token])){
-            //Un Authenticated User Logged In or Token is Changed
-            [self StartCognito:token];
-            credentialsProvider = [[[AWSCognito defaultCognito] configuration] credentialsProvider];
-
-        }
-    }
-
-    [[credentialsProvider refresh] continueWithBlock:^id(AWSTask *task) {
-
-        if (task.error) {
-            NSString *errorType = [task.error.userInfo valueForKey: @"__type"];
-            if([errorType isEqualToString: @"NotAuthorizedException"]){
-                [[AWSCognito defaultCognito] wipe];
-                [credentialsProvider clearKeychain];
-            }
-            NSLog(@"Error in Cognito credentialsprovider.refresh", task.error);
-            reject(@"Error", @"Failed to get Cognito", task.error);
-        }
-        else {
-            NSInteger timeStamp = round(credentialsProvider.expiration.timeIntervalSince1970);
-            resolve( @{@"accessKey": credentialsProvider.accessKey, @"secretKey": credentialsProvider.secretKey, @"sessionKey":credentialsProvider.sessionKey, @"expiration":[NSNumber numberWithInt:timeStamp],
-                       @"cognitoId":credentialsProvider.identityId, @"isAuthenticated":[NSNumber numberWithBool: isAuthenticated]});
-        }
-        return nil;
-    }];
-
-}
-
-
 RCT_EXPORT_METHOD(syncData: (NSString *)datasetName
                   : (NSString *)key
                   : (NSString *)value
                   : (RCTResponseSenderBlock)callback) {
-    AWSCognito *syncClient = [AWSCognito defaultCognito];
-    AWSCognitoDataset *dataset = [syncClient openOrCreateDataset:datasetName];
+  AWSCognito *syncClient = [AWSCognito defaultCognito];
+  AWSCognitoDataset *dataset = [syncClient openOrCreateDataset:datasetName];
 
-    [dataset setString:value forKey:key];
-    [[dataset synchronize] continueWithBlock:^id(AWSTask *task) {
-        if (task.error) {
-            callback(@[ @{@"code":[NSNumber numberWithLong:task.error.code], @"domain":task.error.domain, @"userInfo":task.error.userInfo, @"localizedDescription":task.error.localizedDescription} ]);
-        } else {
-            callback(@[ [NSNull null] ]);
-        }
-        return nil;
-    }];
+  [dataset setString:value forKey:key];
+  [[dataset synchronize] continueWithBlock:^id(AWSTask *task) {
+    if (task.error) {
+      callback(@[ @{@"code":[NSNumber numberWithLong:task.error.code], @"domain":task.error.domain, @"userInfo":task.error.userInfo, @"localizedDescription":task.error.localizedDescription} ]);
+    } else {
+      callback(@[ [NSNull null] ]);
+    }
+    return nil;
+  }];
 }
 
 RCT_EXPORT_METHOD(subscribe: (NSString *)datasetName
                   : (RCTResponseSenderBlock)callback) {
-    AWSCognito *syncClient = [AWSCognito defaultCognito];
-    AWSCognitoDataset *dataset = [syncClient openOrCreateDataset:datasetName];
+  AWSCognito *syncClient = [AWSCognito defaultCognito];
+  AWSCognitoDataset *dataset = [syncClient openOrCreateDataset:datasetName];
 
-    [[dataset subscribe] continueWithBlock:^id(AWSTask *task) {
-        if (task.error) {
-            NSLog(@"Unable to subscribe to dataset");
-            callback(@[ [task.error localizedDescription] ]);
-        } else {
-            NSLog(@"Subscribed to dataset");
-            callback(@[ [NSNull null] ]);
-        }
-        return nil;
-    }];
+  [[dataset subscribe] continueWithBlock:^id(AWSTask *task) {
+    if (task.error) {
+      NSLog(@"Unable to subscribe to dataset");
+      callback(@[ [task.error localizedDescription] ]);
+    } else {
+      NSLog(@"Subscribed to dataset");
+      callback(@[ [NSNull null] ]);
+    }
+    return nil;
+  }];
 }
 
 @end
